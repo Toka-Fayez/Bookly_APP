2107e869b6d6593f57140cfc55c46241883c2075
Toka Fayez <tokafayez@gmail.com>
2025-02-06 05:46:51 +0200

    search view done

--- lib/feateures/search/presentation/views/widgets/search_view_body.dart
+++ lib/feateures/search/presentation/views/widgets/search_view_body.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'custom_search_text_field.dart';
+import 'search_result_list_view.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 30.0),
+      child: Column(
+        children: [
+          CustomSearchTextField(),
+          SizedBox(height: 20),
+          Expanded(
+            child: SearchResultListView(),
+          ),
+        ],
+      ),
+    );
   }
 }
